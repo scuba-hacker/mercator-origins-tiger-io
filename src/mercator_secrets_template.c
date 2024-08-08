e8ec2b782f0c334acc60d407895fafc99daa35b8
Mark B. Jones aka The Scuba Hacker <131517566+scuba-hacker@users.noreply.github.com>
2024-08-08 13:14:12 +0100

    Update mercator_secrets_template.c
    Remove redundant config

--- src/mercator_secrets_template.c
+++ src/mercator_secrets_template.c
@@ -14,24 +14,4 @@ static const char* ssid_3     = "";
 static const char* password_3 = "";
 static const char* label_3 = "";
 static const int   timeout_3 = 10000;
-
-static const char* qubitro_host = "";
-static const int   qubitro_port = 1883;	// 1883 (dev) or 8883 (prod)
-static const char* qubitro_username = "";
-static const char* qubitro_password = "";
-static const char* qubitro_client_id = "";  
-static const char* qubitro_device_id = "";
-static const char* qubitro_device_token = "";
-
-static const char* smtpServer = "";
-static const int   smtpPort = 465;		// depends upon smtp protocol used by your isp
-static const char* smtpSenderEmail = "";
-static const char* smtpSenderPassword = "";
-static const char* smtpRecepientEmail = "";
-
-static const char* twitterConsumerKey = "";
-static const char* twitterConsumerSecret = "";
-static const char* twitterAccessToken = "";
-static const char* twitterAccessTokenSecret = "";
-static const char* twitterBearerToken = "";
 */
