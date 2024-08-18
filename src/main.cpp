7033dbd0b708190cabbddbf9a440523cf1b45394
Mark B. Jones aka The Scuba Hacker <131517566+scuba-hacker@users.noreply.github.com>
2024-08-18 09:04:07 +0100

    Changed AMP Voltage to Purple

--- src/main.cpp
+++ src/main.cpp
@@ -1056,7 +1056,7 @@ void vfd_3_line_clock()
     }
     
     M5.Lcd.setCursor(12, mode_label_y_offset+10);
-    M5.Lcd.setTextColor(TFT_YELLOW, TFT_BLACK);
+    M5.Lcd.setTextColor(TFT_PURPLE, TFT_BLACK);
     M5.Lcd.printf("AXP %.1fC",M5.Axp.GetTempInAXP192());
     
     // M5.Lcd.print("Time");
